fa77da82f1e0893bfaecde1a97f5ae34adc472f9
Ai Nguyen <ainguyenkaka@gmail.com>
2018-05-30 15:27:06 +0800

    run npm eject

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import App from './App';
+import store from './store';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
+  <Provider store={store}>
     <BrowserRouter>
-      <App />
-    </BrowserRouter>, div);
+      <App/>
+    </BrowserRouter>
+  </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/pages/home/HomePage.test.js
+++ src/pages/home/HomePage.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HomePage from './HomePage';
+import { Provider } from 'react-redux'
+import store from '../../store';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<HomePage />, div);
+  ReactDOM.render(
+  <Provider store={store}>
+    <HomePage />
+  </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
